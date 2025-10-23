5288cfa56e921ee4b57a56af4d7a0d35f6ef1369
Mike Rodriguez <69970540+mikerb95@users.noreply.github.com>
2025-10-22 21:33:49 -0500

    Actualizar baseURL en configuración y mejorar el formulario de agregar producto con clases de estilo

--- app/Views/producto/agregar.php
+++ app/Views/producto/agregar.php
@@ -2,10 +2,10 @@
 <?php echo $this->section('contenido') ?>
 
 <h1>Agregar Producto</h1>
-<form action="" method="post">
-    <div>
+<form action="" method="post" class="p-4 border shadow-sm bg-light">
+    <div class="mb-3">
         <label for="nombre">Nombre del producto:</label>
-        <input type="text" name="nombrepro" required>
+        <input type="text" name="nombrepro" required class="form-control" placeholder="Ingrese el nombre ej: cuaderno">
  
     </div>
     <div>
