8faa96194bd01d694b9eb9dcca9480c5bf0e6fb2
Mike Rodriguez <69970540+mikerb95@users.noreply.github.com>
2025-10-22 20:58:41 -0500

    Agregar ruta para el método 'agregar' en el controlador Productos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/productos', 'Productos::index');
+$routes->get('/', 'Home::index'); // Default route
+$routes->get('/productos', 'Productos::index'); // New route for Productos controller
+$routes->get('/productos/agregar', 'Productos::agregar'); // New route for adding products
